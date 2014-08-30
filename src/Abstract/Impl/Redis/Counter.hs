ea982a50ca163b95a95cfa97d9a497c4b81e9199
Andrew Darqui <andrew.darqui@gmail.com>
2014-08-30 12:32:29 -0400

    connect

--- src/Abstract/Impl/Redis/Counter.hs
+++ src/Abstract/Impl/Redis/Counter.hs
@@ -1,5 +1,7 @@
 module Abstract.Impl.Redis.Counter (
  Counter (..),
+ counterRedisWrapper'Int,
+ defaultCounterRedisWrapper'Int,
  mkCounter'Redis'Int,
  mkCounter'Redis'Int'Inc,
  mkCounter'Redis'Int'Dec,
@@ -7,7 +9,7 @@ module Abstract.Impl.Redis.Counter (
 ) where
 
 import Abstract.Interfaces.Counter
-import Abstract.Impl.Redis.Counter.Internal (mkCounter'Redis'Int)
+import Abstract.Impl.Redis.Counter.Internal (mkCounter'Redis'Int, defaultCounterRedisWrapper'Int, counterRedisWrapper'Int)
 import Abstract.Impl.Redis.Counter.Inc (mkCounter'Redis'Int'Inc)
 import Abstract.Impl.Redis.Counter.Dec (mkCounter'Redis'Int'Dec)
 import Abstract.Impl.Redis.Counter.Get (mkCounter'Redis'Int'Get)
